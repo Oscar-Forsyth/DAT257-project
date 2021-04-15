4167cd86aa58d73b15b444914502945138cf59f6
Hannes Svahn <57436847+Hemlos@users.noreply.github.com>
2021-04-15 10:45:43 +0200

    test2

--- Application/app/src/main/java/com/example/application/MainActivity.java
+++ Application/app/src/main/java/com/example/application/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         System.out.println("Hej testtest");
-        System.out.println("test2");
+        System.out.println("test3");
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
