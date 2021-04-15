8b54a22cd93c0e1427982064a0f80687f8969a49
Mantas Trakanavicius <mantastrakanavicius@gmail.com>
2021-04-15 19:54:42 +0200

    Development of top bar
    Minor bug fixes and further development of top bar...

--- Application/app/src/main/java/com/example/application/MainMenu.java
+++ Application/app/src/main/java/com/example/application/MainMenu.java
@@ -1,13 +1,12 @@
 package com.example.application;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
-import android.widget.Toolbar;
 
 public class MainMenu extends AppCompatActivity {
 
-    private Toolbar toolbarTop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
