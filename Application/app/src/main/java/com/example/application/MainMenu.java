a6ca5e342a469db8820d3407858c5c7aa1c5806d
simonindek <simonschuster3@gmail.com>
2021-04-17 11:27:50 +0200

    Intents blueprint main menu

--- Application/app/src/main/java/com/example/application/MainMenu.java
+++ Application/app/src/main/java/com/example/application/MainMenu.java
@@ -3,7 +3,10 @@ package com.example.application;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -17,12 +20,44 @@ public class MainMenu extends AppCompatActivity {
         setSupportActionBar(toolbar);
     }
 
-    /** Called when the user taps the 'Recommended Sports' button */
-    /**public void uponRecommendedSports(View)
 
-    /** Called when the user taps the 'Upcoming Events' button */
+    /** INTENTS TO OPEN NEW ACTIVITIES FROM THE MAIN MENU:
 
-    /** Called when the user taps the 'Sports & Committees' button */
 
-    /** Called when the user tapts the 'Challenges button' */
+  /*  /** Called when the user taps the 'Recommended Sports' button *//*
+    public void openRecommendedSports(View view) {
+        Intent intent = new Intent(this, RecommendedSports.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    *//** Called when the user taps the 'Upcoming Events' button *//*
+    public void openUpcomingEvents(View view) {
+        Intent intent = new Intent(this, UpcomingEvents.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    *//** Called when the user taps the 'Sports & Committees' button *//*
+    public void openSportsAndCommittees(View view) {
+        Intent intent = new Intent(this, SportsAndCommittees.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    *//** Called when the user taps the 'Challenges button' *//*
+    public void openChallenges(View view) {
+        Intent intent = new Intent(this, Challenges.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }*/
+
 }
