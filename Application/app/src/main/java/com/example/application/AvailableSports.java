165675dd01246c7ca84b8f38d794fd50f5e2ac5e
OscarJohansson1 <johaosc@student.chalmers.se>
2021-04-17 15:33:21 +0200

    Made Fragment AvailableSports to an activity

--- Application/app/src/main/java/com/example/application/AvailableSports.java
+++ Application/app/src/main/java/com/example/application/AvailableSports.java
@@ -1,17 +1,11 @@
 package com.example.application;
 
-import android.os.Build;
-import android.os.Bundle;
-
-import androidx.annotation.RequiresApi;
-import androidx.fragment.app.Fragment;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -23,77 +17,29 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.w3c.dom.ls.LSOutput;
 
-import java.io.File;
-import java.io.InputStream;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AvailableSports#newInstance} factory method to
- * create an instance of this fragment.
- */
-@RequiresApi(api = Build.VERSION_CODES.O)
-public class AvailableSports extends Fragment {
-
+public class AvailableSports extends AppCompatActivity {
     RecyclerView recyclerView;
     List<Sport> sports;
-    InputStream is = getResources().openRawResource(R.raw.sports);
-    File file = new File("android.resource://raw/sports.json");
+    private static String JSON_URL = "http://www.json-generator.com/api/json/get/cpFoKWPwbS?indent=2";
     Adapter adapter;
 
-
-
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public AvailableSports() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AvailableSports.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AvailableSports newInstance() {
-        AvailableSports fragment = new AvailableSports();
-        return fragment;
-    }
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-        
-        recyclerView = getActivity().findViewById(R.id.sportsList);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.sportsList);
         sports = new ArrayList<>();
-        extractSports();
-        
-        
+        extractSongs();
     }
 
-    private void extractSports(){
-        RequestQueue queue = Volley.newRequestQueue(getActivity().getApplicationContext());
-        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, file.toString(), null, new Response.Listener<JSONArray>() {
+    private void extractSongs() {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, JSON_URL, null, new Response.Listener<JSONArray>() {
             @Override
             public void onResponse(JSONArray response) {
                 for (int i = 0; i < response.length(); i++) {
@@ -104,7 +50,6 @@ public class AvailableSports extends Fragment {
                         sport.setName(sportObject.getString("name").toString());
                         sport.setDescription(sportObject.getString("description".toString()));
                         sport.setLogo(sportObject.getString("logo"));
-
                         sports.add(sport);
 
                     } catch (JSONException e) {
@@ -112,8 +57,8 @@ public class AvailableSports extends Fragment {
                     }
                 }
 
-                recyclerView.setLayoutManager(new LinearLayoutManager(getActivity().getApplicationContext()));
-                adapter = new Adapter(getActivity().getApplicationContext(),sports);
+                recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                adapter = new Adapter(getApplicationContext(),sports);
                 recyclerView.setAdapter(adapter);
             }
         }, new Response.ErrorListener() {
@@ -125,15 +70,5 @@ public class AvailableSports extends Fragment {
 
         queue.add(jsonArrayRequest);
 
-    }
-
-
-
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_available_sports, container, false);
     }
 }
