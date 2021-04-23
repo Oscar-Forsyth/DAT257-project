2c419d98a2a5436a66a352ed995f7d1bc985c3aa
Oscar Forsyth <44546586+goatlover@users.noreply.github.com>
2021-04-23 16:18:32 +0200

    kindofworks

--- Application/app/src/main/java/com/example/application/Wizard.java
+++ Application/app/src/main/java/com/example/application/Wizard.java
@@ -6,9 +6,12 @@ import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 import android.text.Html;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class Wizard extends AppCompatActivity {
@@ -22,18 +25,37 @@ public class Wizard extends AppCompatActivity {
     private Button mWizardBackButton;
 
     private int mCurrentPage;
+
+    private RadioButton radioButton1;
+    private RadioButton radioButton2;
+    private RadioButton radioButton3;
+    private RadioGroup radioGroup;
+    private RadioButton selectedRadioButton;
+
+    View v;
+
+    private final int NUMBER_OF_QUESTIONS = 3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wizard);
 
-        mSlideViewPager = (ViewPager2) findViewById(R.id.slideView);
-        mDotLayout = (LinearLayout) findViewById(R.id.dotsLayout);
-
-        mWizardNextButton = (Button) findViewById(R.id.wizardNextButton);
-        mWizardBackButton = (Button) findViewById(R.id.wizardBackButton);
-
         wizardAdapter = new WizardAdapter(this);
+
+        v = LayoutInflater.from(this).inflate(R.layout.wizard_layout, null);
+
+        radioButton1 = v.findViewById(R.id.radioButton1);
+        radioButton2 = v.findViewById(R.id.radioButton2);
+        radioButton3 = v.findViewById(R.id.radioButton3);
+
+
+
+        mSlideViewPager = findViewById(R.id.slideView);
+        mDotLayout = findViewById(R.id.dotsLayout);
+
+        mWizardNextButton = findViewById(R.id.wizardNextButton);
+        mWizardBackButton = findViewById(R.id.wizardBackButton);
 
         mSlideViewPager.setAdapter(wizardAdapter);
 
@@ -47,28 +69,57 @@ public class Wizard extends AppCompatActivity {
                 mSlideViewPager.setCurrentItem(mCurrentPage-1);
             }
         });
+
         mWizardNextButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                mSlideViewPager.setCurrentItem(mCurrentPage+1);
+
+                int id= wizardAdapter.getRadioButton(mCurrentPage);
+                RadioButton rb = findViewById(id);
+
+                if(id!=-1){
+                    System.out.println("text:" + rb.getText().toString());
+                }
+
+                if(mCurrentPage == NUMBER_OF_QUESTIONS -1 ){
+                    //Pressed finished
+                    //TODO
+                }
+                else{
+                    mSlideViewPager.setCurrentItem(mCurrentPage+1);
+                }
+
             }
         });
+        /*
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton radioButton =  group.findViewById(checkedId);
+                if(radioButton != null && checkedId != -1){
+                    System.out.println(radioButton.getText().toString());
+                }
+            }
+        });
+
+         */
+
     }
 
     public void addDotsIndicator(int position){
         mDotLayout.removeAllViews();
-        mDots = new TextView[3];
+        mDots = new TextView[NUMBER_OF_QUESTIONS];
 
         for (int i = 0; i < mDots.length; i++) {
             mDots[i] = new TextView(this);
             mDots[i].setText(Html.fromHtml("&#8226;"));
             mDots[i].setTextSize(35);
-            mDots[i].setTextColor(getResources().getColor(R.color.white));
+            mDots[i].setTextColor(getResources().getColor(R.color.darker_white));
 
             mDotLayout.addView(mDots[i]);
         }
         if(mDots.length > 0){
-            mDots[position].setTextColor(getResources().getColor(R.color.black));
+            mDots[position].setTextColor(getResources().getColor(R.color.white));
         }
     }
 
@@ -82,6 +133,8 @@ public class Wizard extends AppCompatActivity {
         public void onPageSelected(int position) {
             addDotsIndicator(position);
             mCurrentPage = position;
+
+
 
             if(position == 0){
 
@@ -113,4 +166,5 @@ public class Wizard extends AppCompatActivity {
 
         }
     };
+
 }
--- Application/app/src/main/java/com/example/application/WizardAdapter.java
+++ Application/app/src/main/java/com/example/application/WizardAdapter.java
@@ -5,15 +5,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class WizardAdapter extends RecyclerView.Adapter<WizardAdapter.WizardViewHolder> {
 
     Context context;
     LayoutInflater layoutInflater;
+    RadioGroup radioGroup;
+    View view;
+    Map<Integer, WizardViewHolder> map = new HashMap<>();
+    int count= 0;
 
     public WizardAdapter(Context context) {
         this.context = context;
@@ -25,26 +33,47 @@ public class WizardAdapter extends RecyclerView.Adapter<WizardAdapter.WizardView
             "This is question 3"
     };
 
-    public String[] wizardRadioButton1 = {
+    public String[] radioButtonGroup1 = {
             "option 1, question 1 ",
-            "option 2, question 1",
-            "option 3, question 1"
+            "option 1, question 2",
+            "option 1, question 3"
     };
-    public String[] wizardRadioButton2 = {
-            "option 1, question 2 ",
+    public String[] radioButtonGroup2 = {
+            "option 2, question 1 ",
             "option 2, question 2",
-            "option 3, question 2"
+            "option 2, question 3"
     };
-    public String[] wizardRadioButton3 = {
-            "option 1, question 3 ",
-            "option 2, question 3",
+    public String[] radioButtonGroup3 = {
+            "option 3, question 1 ",
+            "option 3, question 2",
             "option 3, question 3"
     };
     public String[][] wizardRadioButtons = {
-            wizardRadioButton1,
-            wizardRadioButton2,
-            wizardRadioButton3
+            radioButtonGroup1,
+            radioButtonGroup2,
+            radioButtonGroup3
     };
+
+    public int getRadioButton(int key){
+        WizardViewHolder wizardViewHolder = map.get(key);
+        System.out.println(map.toString());
+        RadioGroup radioGroup = wizardViewHolder.getRadioGroup();
+        int result = radioGroup.getCheckedRadioButtonId();
+        /*
+        RadioButton selectedRadioButton;
+            if(result != -1){
+                selectedRadioButton = view.findViewById(result);
+                String RBText = selectedRadioButton.getText().toString();
+                System.out.println(RBText);
+            }else {
+                System.out.println("Nothing selected");
+            }
+
+
+         */
+        return result;
+    }
+
 
     @NonNull
     @Override
@@ -52,18 +81,20 @@ public class WizardAdapter extends RecyclerView.Adapter<WizardAdapter.WizardView
         //container.removeView((RelativeLayout) object);
         layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
         View view = layoutInflater.inflate(R.layout.wizard_layout, parent, false);
-        return new WizardViewHolder(view);
+        WizardViewHolder wizardViewHolder = new WizardViewHolder(view);
+        map.put(count,wizardViewHolder);
+        count++;
+        return wizardViewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull WizardViewHolder holder, int position) {
+
+        view = holder.itemView;
         holder.wizardQuestion.setText(wizardQuestions[position]);
-        holder.radioButton1.setText(wizardRadioButtons[position][0]);
-        holder.radioButton2.setText(wizardRadioButtons[position][1]);
-        holder.radioButton3.setText(wizardRadioButtons[position][2]);
-
-        //container.addView(view);
-
+        holder.radioButton1.setText(wizardRadioButtons[0][position]);
+        holder.radioButton2.setText(wizardRadioButtons[1][position]);
+        holder.radioButton3.setText(wizardRadioButtons[2][position]);
     }
 
     @Override
@@ -75,31 +106,26 @@ public class WizardAdapter extends RecyclerView.Adapter<WizardAdapter.WizardView
     /**
      * assigns values to and holds attributes necessary for the items in recyclerView
      */
-    public class WizardViewHolder extends RecyclerView.ViewHolder {
+    public static class WizardViewHolder extends RecyclerView.ViewHolder {
 
         TextView wizardQuestion;
         RadioButton radioButton1, radioButton2, radioButton3;
+        RadioGroup radioGroup;
 
 
         public WizardViewHolder(@NonNull View itemView) {
             super(itemView);
 
             wizardQuestion = itemView.findViewById(R.id.wizardQuestion);
-            radioButton1 = itemView.findViewById(R.id.wizardRadioButton1);
-            radioButton2 = itemView.findViewById(R.id.wizardRadioButton2);
-            radioButton3 = itemView.findViewById(R.id.wizardRadioButton3);
+            radioButton1 = itemView.findViewById(R.id.radioButton1);
+            radioButton2 = itemView.findViewById(R.id.radioButton2);
+            radioButton3 = itemView.findViewById(R.id.radioButton3);
+            radioGroup = itemView.findViewById(R.id.radioGroup);
 
+        }
 
-            // handle onClick
-            /*
-            itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Toast.makeText(v.getContext(), "Do Something With this Click", Toast.LENGTH_SHORT).show();
-                }
-            });
-
-             */
+        public RadioGroup getRadioGroup() {
+            return radioGroup;
         }
     }
 }
