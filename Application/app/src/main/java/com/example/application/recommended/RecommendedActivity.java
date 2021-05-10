1927ce16af9f1fef94a82ffceaebf48b727478d4
Mantas Trakanavicius <mantastrakanavicius@gmail.com>
2021-05-10 08:47:17 +0200

    Added TODOs
    I did a code review of classes Animations.java and RecommendedActivity.java. Added some todos also.

--- Application/app/src/main/java/com/example/application/recommended/RecommendedActivity.java
+++ Application/app/src/main/java/com/example/application/recommended/RecommendedActivity.java
@@ -1,26 +1,30 @@
 package com.example.application.recommended;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.FragmentManager;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
+
 import com.example.application.R;
 
+//TODO add explaining comment what this class should do
 public class RecommendedActivity extends AppCompatActivity {
 
     private boolean TAKEN_QUIZ;
     private Toolbar toolbar;
     private TextView textView;
 
+    //TODO add comment explaining what this method does
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //TODO remove the line below if it is unnecessary
         //setContentView(R.layout.activity_recommended);
 
 
@@ -28,6 +32,7 @@ public class RecommendedActivity extends AppCompatActivity {
         SharedPreferences prefs = getSharedPreferences("Save", MODE_PRIVATE);
         TAKEN_QUIZ = prefs.getBoolean("takenQuiz", false);
 
+        // TODO explain what happens here?
         if(TAKEN_QUIZ){
             setContentView(R.layout.activity_recommended_empty);
             FragmentManager fm = getSupportFragmentManager();
@@ -40,11 +45,13 @@ public class RecommendedActivity extends AppCompatActivity {
         textView.setText("Recommended Sports");
     }
 
+    //TODO add comment explaining the method here
     public void openQuizWizard(View view) {
         Intent intent = new Intent(this, Wizard.class);
         startActivity(intent);
     }
 
+    //TODO add comment explaining the method here
     public void goBack(View view){
         this.onBackPressed();
     }
